38f5af05a75e996289d6b596b7b58c89bd64ae5c
rasyidrmhd <rasyidrmuhammad@gmail.com>
2025-06-12 09:11:49 +0700

    using simplest index

--- api/index.js
+++ api/index.js
@@ -1,11 +1,22 @@
 require("dotenv").config();
 
 const express = require("express");
+const { v2 } = require("cloudinary");
+const fetcher = require("node-fetch");
+const bodyParser = require("body-parser");
 const serverless = require("serverless-http");
 
+v2.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+const urlencodedParser = bodyParser.urlencoded({ extended: true });
 
 app.get("/", (req, res) => res.send("Just webhook"));
 
@@ -13,5 +24,6 @@ app.get("/health", (req, res) => {
   return res.status(200).send("This webhook is 100% healthy (maybe)");
 });
 
+app.listen(PORT, () => console.log(`Server run on: ${PORT}`));
+
 module.exports = app;
-module.exports.handler = serverless(app);
